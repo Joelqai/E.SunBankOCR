61b81b9fe426e726d25a26b70f769550d0ab8d2a
謝皇廷 <timshieh@awoo.com.tw>
2021-09-28 00:36:03 +0800

    [FIX] colab cannot use set_memory_growth

--- HandWritingReconition/evaluate/__init__.py
+++ HandWritingReconition/evaluate/__init__.py
@@ -20,10 +20,10 @@ def test_step(images, labels, model, loss_object, test_loss, test_accuracy):
 
 def evaluation(config):
     # GPU settings
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
+    # gpus = tf.config.experimental.list_physical_devices('GPU')
+    # if gpus:
+    #     for gpu in gpus:
+    #         tf.config.experimental.set_memory_growth(gpu, True)
 
     print("開始進行測試~~~~~")
     # get the original_dataset
