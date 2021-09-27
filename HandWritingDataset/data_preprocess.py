ea9f9004f247b987010ce703065bdbabe8f78187
謝皇廷 <timshieh@awoo.com.tw>
2021-09-27 20:35:49 +0800

    add patch data preprocess

--- HandWritingDataset/data_preprocess.py
+++ HandWritingDataset/data_preprocess.py
@@ -45,11 +45,50 @@ class ESunHandWritingPreprocess(AllocationDataToDifferentFolder):
 
         #撰寫匹配規則，將檔案分配對應的資料夾
         for filename in filenames:
-            if not os.path.isfile(os.path.join(os.path.join(self.destination_folder_path,vocab_dict.get(filename[-5])),filename)):
-                src_complete_path = os.path.join(self.source_folder_path,filename)
-                dst_complete_path = os.path.join(self.destination_folder_path,vocab_dict.get(filename[-5]))
-                shutil.copy(src=src_complete_path, dst=dst_complete_path)
-                print(f"add file {filename}")
+            try:
+                if not os.path.isfile(os.path.join(os.path.join(self.destination_folder_path,vocab_dict.get(filename[-5])),filename)):
+                    src_complete_path = os.path.join(self.source_folder_path,filename)
+                    dst_complete_path = os.path.join(self.destination_folder_path,vocab_dict.get(filename[-5]))
+                    shutil.copy(src=src_complete_path, dst=dst_complete_path)
+                    print(f"add file {filename}")
+            except:
+                print(filename,"out of class")
+
+class ESunHandWritingPatchPreprocess(AllocationDataToDifferentFolder):
+
+    def __init__(self, source_folder_path, class_num, word_dictionary_path, destination_folder_path):
+        super().__init__(source_folder_path, destination_folder_path, class_num)
+        self.word_dictionary_path = word_dictionary_path
+        
+    def execute(self):
+        #讀取所有檔案列表
+        filenames = os.listdir(self.source_folder_path)
+
+        #創建資料夾(根據官方字典去建立對應的資料夾)
+        vocab_dict = {}
+        count = 0
+        with open(self.word_dictionary_path, "r") as fp:
+            for line in fp.readlines():
+                line = line.replace("\n","")
+                vocab_dict[line]=str(count)
+                count = count+1
+        if not os.path.isdir(self.destination_folder_path):
+            os.mkdir(self.destination_folder_path)
+        for word, index in vocab_dict.items():
+            complete_path = os.path.join(self.destination_folder_path,index)
+            if not os.path.isdir(complete_path):
+                os.mkdir(complete_path)
+
+        #撰寫匹配規則，將檔案分配對應的資料夾
+        for filename in filenames:
+            try:
+                if not os.path.isfile(os.path.join(os.path.join(self.destination_folder_path,vocab_dict.get(filename[0])),filename)):
+                    src_complete_path = os.path.join(self.source_folder_path,filename)
+                    dst_complete_path = os.path.join(self.destination_folder_path,vocab_dict.get(filename[0]))
+                    shutil.copy(src=src_complete_path, dst=dst_complete_path)
+                    print(f"add file {filename}")
+            except:
+                print(filename,"out of class")
 
 if __name__ == "__main__":
     e_sun = ESunHandWritingPreprocess(source_folder_path="/Users/timshieh/Documents/HandWritingCategory/origin_data/玉山人工智慧公開挑戰賽2021夏季賽/train", 
