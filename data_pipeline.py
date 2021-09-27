bbceabe8ef5922a5d82066f0f43ac7cb314e7850
謝皇廷 <timshieh@awoo.com.tw>
2021-09-27 20:50:12 +0800

    [UPDATE] add data augument

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,13 +1,11 @@
 import os 
-from HandWritingDataset.data_preprocess import ESunHandWritingPreprocess
+from HandWritingDataset.add_handmake_data import DataAugment
+from HandWritingDataset.data_preprocess import ESunHandWritingPreprocess,ESunHandWritingPatchPreprocess
 from HandWritingDataset.split_dataset import SplitDataset
 
 
-def main():
-        
-
-
-
+def main(patch_training_data):
+    
     # data preprocessing:將一整包資料按照類別分成對應到資料夾(資料夾皆用數字編號)
     e_sun = ESunHandWritingPreprocess(source_folder_path="/Users/timshieh/Documents/HandWritingCategory/origin_data/玉山人工智慧公開挑戰賽2021夏季賽/train", 
                                       word_dictionary_path="/Users/timshieh/Documents/HandWritingCategory/origin_data/玉山人工智慧公開挑戰賽2021夏季賽/training data dic.txt",
@@ -22,10 +20,30 @@ def main():
 
     split_dataset = SplitDataset(dataset_dir=dataset_dir,
                                  saved_dataset_dir=saved_dataset_dir,
-                                 train_ratio=0.8, 
+                                 train_ratio=0.7, 
                                  test_ratio=0.15,
                                  show_progress=True)
     split_dataset.start_splitting()
+    
+    #是否要融入手作資料 將train跟patch資料整併
+    if patch_training_data:
+        #將原始資料影像處理成訓練資料
+        source_folder = "/Users/timshieh/Documents/HandWritingCategory/cleaned_data_50_50"
+        data_augment = DataAugment(source_folder=source_folder, destination_folder='/Users/timshieh/Documents/Reconition_Github/E.SunBankOCR/patch_data')
+        data_augment.pipeline()
+        #將資料切割1-800資料夾
+        e_sun = ESunHandWritingPatchPreprocess(source_folder_path="/Users/timshieh/Documents/Reconition_Github/E.SunBankOCR/patch_data", 
+                                        word_dictionary_path="/Users/timshieh/Documents/HandWritingCategory/origin_data/玉山人工智慧公開挑戰賽2021夏季賽/training data dic.txt",
+                                        destination_folder_path="/Users/timshieh/Documents/Reconition_Github/E.SunBankOCR/train_data/patch",
+                                        class_num=800)
+        e_sun.execute()
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    main()
+    patch_training_data = True
+    main(patch_training_data)
